9aa4fd6f8c5d195a05569becdf66260506fad5f9
Zac Flamig <Zac@WeatherWary.com>
2018-02-26 23:24:09 -0600

    chore(test): add some asserts

--- tests/test_get.py
+++ tests/test_get.py
@@ -1,9 +1,13 @@
 
-def test_get_bloodpac():
+def test_get_matsu():
     from doiclient.client import DOIClient
     
     signpost = DOIClient(baseurl="https://dx.doi.org/")
     
     res = signpost.get("10.1007/s41060-017-0052-3")
-    print(res.to_json())
+    res = res.to_json()
+    assert res["urls"] == ["http://link.springer.com/article/10.1007/s41060-017-0052-3/fulltext.html", "http://link.springer.com/content/pdf/10.1007/s41060-017-0052-3.pdf", "http://link.springer.com/content/pdf/10.1007/s41060-017-0052-3.pdf"]
+    assert res["did"] == "10.1007/s41060-017-0052-3"
+    assert res["rev"] == ""
+    assert res["size"] == ""
 
